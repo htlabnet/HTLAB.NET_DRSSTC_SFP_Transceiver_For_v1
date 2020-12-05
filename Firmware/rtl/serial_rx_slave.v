bb6fa8b300c8d2980d027e0c577ef58622b7c242
kingyoPiyo <kingyo.no.esa1192@gmail.com>
2020-12-05 12:47:10 +0900

    rename

--- Firmware/rtl/serial_rx_slave.v
+++ Firmware/rtl/serial_rx_slave.v
@@ -1,7 +1,7 @@
 /*============================================================================*/
 /*
- * @file    serial_rx.v
- * @brief   Serial data recieve & decode module
+ * @file    serial_rx_slave.v
+ * @brief   Serial data recieve & decode module for slave CPLD
  * @note    Sampling rate    : 40MHz
             Serial data rate : 10Mbps
  * @date    2020/11/28
@@ -9,7 +9,7 @@
  */
 /*============================================================================*/
 
-module serial_rx (
+module serial_rx_slave (
     input   wire            i_clk,      // 40MHz
     input   wire            i_res_n,
 
@@ -26,13 +26,17 @@ module serial_rx (
     output  reg     [2:0]   o_Option,
 
     // Status LED
-    output  wire    [1:0]   o_rx_led
+    output  wire    [1:0]   o_rx_led,
+
+    // Rx Error count
+    output  reg     [15:0]  o_err_cnt
 );
 
     assign o_rcv_en_n = 1'b0;   // Always enable
 
     wire            w_sync1bData;
     wire            w_sync1bEn;
+    wire            w_cdr_err;
 
     // CDR
     cdr cdr_inst (
@@ -40,7 +44,8 @@ module serial_rx (
         .i_res_n ( i_res_n ),
         .i_SerialData ( i_SerialData ),
         .o_RecoveryData ( w_sync1bData ),
-        .o_DataEn ( w_sync1bEn )
+        .o_DataEn ( w_sync1bEn ),
+        .o_err ( w_cdr_err )
     );
 
     // 10bit shift register
@@ -85,6 +90,8 @@ module serial_rx (
         if (~i_res_n) begin
             r_sym_locked <= 1'b0;
             r_k28_5_cnt <= 16'hFFFF;
+        end else if (w_cdr_err) begin
+            r_sym_locked <= 1'b0;
         end else if (w_sync1bEn) begin
             if (w_k28_5_det) begin
                 r_k28_5_cnt <= 16'd0;
@@ -113,16 +120,27 @@ module serial_rx (
     reg             r_code_err;
     wire            w_disp_err;
     reg             r_disp_err;
+    reg             r_sym_locked_old;
     always @(posedge i_clk or negedge i_res_n) begin
         if (~i_res_n) begin
             r_disp <= 1'b0;
             r_code_err <= 1'b1;
             r_disp_err <= 1'b1;
+            o_err_cnt <= 16'd0;
+            r_sym_locked_old <= 1'b0;
         end else begin
             if (r_sym_capture) begin
                 r_disp <= w_disp;
                 r_code_err <= w_code_err;
                 r_disp_err <= w_disp_err;
+            end
+
+            // Error counter
+            r_sym_locked_old <= r_sym_locked;
+            if (r_sym_locked_old & ~r_sym_locked) begin
+                if (o_err_cnt != 16'hFFFF) begin
+                    o_err_cnt <= o_err_cnt + 16'd1;
+                end
             end
         end
     end
